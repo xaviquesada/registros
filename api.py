57d43b5b71db757d4e935c1361e1958ffd9f13b0
xaviquesada <xaviquesada@gmail.com>
2023-07-17 08:59:39 +0200

    Remove comments from api.py

--- api.py
+++ api.py
@@ -6,16 +6,10 @@ app = Flask(__name__)
 
 @app.route('/api/v0/<device_id>/<card_id>', methods=['POST'])
 def registrar(device_id, card_id):
-    # Obtener la IP del cliente que realiza la petición
     ip_request = request.remote_addr
-
-    # Crear objeto Registro
     ts = datetime.now()
     registro = Registro(device_id=device_id, card_id=card_id, ts=ts, ip_request=ip_request)
-
-    # Guardar registro en la base de datos
     registro.guardar_registro()
-
     return "",201
 
 if __name__ == '__main__':
